4033ae934a8372edb77357db9500143777b32fcf
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-09-30 15:40:14 +0300

    Refactor: extract WENO subroutines into module.

---
+++ src/advection.py
@@ -0,0 +1,60 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import weno2
+
+
+def flux(val):
+    return val
+
+def flux_deriv(val):
+    return CHAR_SPEED
+
+def max_flux_deriv(a, b):
+    return CHAR_SPEED
+
+def initial_condition(x_center):
+    u0 = np.zeros(N)
+
+    for i in range(0, N):
+        if -0.2 <= x_center[i] <= 0.2:
+            u0[i] = 1.0
+        else:
+            u0[i] = 0.0
+
+    return u0
+
+def exact_solution(x, time):
+    x = np.remainder(x, 2.0)
+    for i in range(N):
+        if x[i] >= 1.0:
+            x[i] -= 2.0
+
+    u_exact = np.zeros(N)
+
+    for i in (range(N)):
+        if -0.2 <= x[i] <= 0.2:
+            u_exact[i] = 1.0
+
+    return u_exact
+
+
+# Number of cells.
+N = 160
+a = -1.0
+b = 1.0
+CHAR_SPEED = 1.0
+T = 4.0
+
+w = weno2.Weno2(a, b, N, T, flux, flux_deriv, max_flux_deriv, CHAR_SPEED)
+x_center = w.get_x_center()
+u0 = initial_condition(x_center)
+
+solution = w.integrate(u0)
+
+plt.plot(x_center, solution, 'o', label='WENO, $k = 2$')
+plt.plot(x_center, exact_solution(x_center - CHAR_SPEED * T, T), label='Exact')
+plt.legend(loc='best')
+plt.ylim([-0.1, 1.1])
+plt.xticks(np.linspace(-1, 1, 11, endpoint=True))
+plt.show()
+# plt.savefig('/home/dima/weno2_advection_N=' + str(N) + '_T=' + str(T) + '.eps')
---
+++ src/weno2.py
@@ -0,0 +1,120 @@
+import numpy as np
+
+class Weno2:
+    def __init__(self, left_boundary, right_boundary, ncells, time, flux_callback, flux_deriv_callback, max_flux_deriv_callback, char_speed, cfl_number=0.8, eps=1.0e-6):
+        """
+
+        :rtype : Weno2
+        """
+        a = left_boundary
+        b = right_boundary
+        self.N = ncells
+        self.T = time
+        self.dx = (b - a) / (self.N + 0.0)
+        CFL_NUMBER = cfl_number
+        CHAR_SPEED = char_speed
+        self.dt = CFL_NUMBER * self.dx / CHAR_SPEED
+        self.t = 0.0
+        ORDER_OF_SCHEME = 2
+        self.EPS = eps
+        self.D0 = 2.0 / 3.0
+        self.D1 = 1.0 / 3.0
+        self.flux = flux_callback
+        self.flux_deriv = flux_deriv_callback
+        self.max_flux_deriv = max_flux_deriv_callback
+        self.x_boundary = np.linspace(a, b, self.N + 1)
+        self.x_center = np.zeros(self.N)
+
+        for i in range(0, self.N):
+            self.x_center[i] = (self.x_boundary[i] + self.x_boundary[i + 1]) / 2.0
+
+        self.u_right_boundary_approx = np.zeros((ORDER_OF_SCHEME, self.N))
+        self.u_left_boundary_approx = np.zeros((ORDER_OF_SCHEME, self.N))
+        self.u_right_boundary = np.zeros(self.N)
+        self.u_left_boundary = np.zeros(self.N)
+        self.beta0 = np.zeros(self.N)
+        self.beta1 = np.zeros(self.N)
+        self.alpha0 = np.zeros(self.N)
+        self.alpha1 = np.zeros(self.N)
+        self.sum_alpha = np.zeros(self.N)
+        self.omega0 = np.zeros(self.N)
+        self.omega1 = np.zeros(self.N)
+        self.fFlux = np.zeros(self.N + 1)
+        self.rhsValues = np.zeros(self.N)
+        self.u_multistage = np.zeros((3, self.N))
+
+
+    def integrate(self, u0):
+        self.u_multistage[0] = u0
+
+        while self.t < self.T:
+            if self.t + self.dt > self.T:
+                self.dt = self.T - self.t
+
+            self.t += self.dt
+            self.u_multistage[1] = self.u_multistage[0] + self.dt * self.rhs(self.u_multistage[0])
+            self.u_multistage[2] = (3 * self.u_multistage[0] + self.u_multistage[1] + self.dt * self.rhs(self.u_multistage[1])) / 4.0
+            self.u_multistage[0] = (self.u_multistage[0] + 2.0 * self.u_multistage[2] + 2.0 * self.dt * self.rhs(self.u_multistage[2])) / 3.0
+
+        return self.u_multistage[0]
+
+
+    def rhs(self, u):
+        # WENO Reconstruction
+        # Approximations for inner cells 0 < i < N-1.
+        self.u_right_boundary_approx[0][1:-1] = 0.5 * u[1:-1] + 0.5 * u[2:]
+        self.u_right_boundary_approx[1][1:-1] = -0.5 * u[0:-2] + 1.5 * u[1:-1]
+        self.u_left_boundary_approx[0][1:-1] = 1.5 * u[1:-1] - 0.5 * u[2:]
+        self.u_left_boundary_approx[1][1:-1] = 0.5 * u[0:-2] + 0.5 * u[1:-1]
+
+        # Approximations for cell i = 0 (the leftmost cell).
+        self.u_right_boundary_approx[0][0] = 0.5 * u[0] + 0.5 * u[1]
+        self.u_right_boundary_approx[1][0]= -0.5 * u[self.N - 1] + 1.5 * u[0]
+        self.u_left_boundary_approx[0][0] = 1.5 * u[0] - 0.5 * u[1]
+        self.u_left_boundary_approx[1][0] = 0.5 * u[self.N - 1] + 0.5 * u[0]
+
+        # Approximations for cell i = N-1 (the rightmost cell).
+        self.u_right_boundary_approx[0][self.N - 1] = 0.5 * u[self.N - 1] + 0.5 * u[0]
+        self.u_right_boundary_approx[1][self.N - 1] = -0.5 * u[self.N - 2] + 1.5 * u[self.N - 1]
+        self.u_left_boundary_approx[0][self.N - 1] = 1.5 * u[self.N - 1] - 0.5 * u[0]
+        self.u_left_boundary_approx[1][self.N - 1] = 0.5 * u[self.N - 2] + 0.5 * u[self.N - 1]
+
+        self.beta0[1:-2] = (u[2:-1] - u[1:-2]) ** 2
+        self.beta1[1:-2] = (u[1:-2] - u[0:-3]) ** 2
+        self.beta0[0] = (u[1] - u[0]) ** 2
+        self.beta1[0] = (u[0] - u[self.N - 1]) ** 2
+        self.beta0[-1] = (u[0] - u[self.N - 1]) ** 2
+        self.beta1[-1] = (u[self.N - 1] - u[self.N - 2]) ** 2
+        self.alpha0 = self.D0 / ((self.EPS + self.beta0) ** 2)
+        self.alpha1 = self.D1 / ((self.EPS + self.beta1) ** 2)
+        self.sum_alpha = self.alpha0 + self.alpha1
+        self.omega0 = self.alpha0 / self.sum_alpha
+        self.omega1 = self.alpha1 / self.sum_alpha
+        self.u_right_boundary = np.multiply(self.omega0, self.u_right_boundary_approx[0][:]) + \
+                           np.multiply(self.omega1, self.u_right_boundary_approx[1][:])
+        self.u_left_boundary = np.multiply(self.omega0, self.u_left_boundary_approx[0][:]) + \
+                          np.multiply(self.omega1, self.u_left_boundary_approx[1][:])
+
+        # Numerical flux calculation.
+        self.fFlux[1:-1] = self.numflux(self.u_right_boundary[0:-1], self.u_left_boundary[1:])
+        self.fFlux[0] = self.numflux(self.u_right_boundary[self.N - 1], self.u_left_boundary[0])
+        self.fFlux[self.N] = self.numflux(self.u_right_boundary[self.N - 1], self.u_left_boundary[0])
+
+        # Right hand side calculation.
+        rhsValues = self.fFlux[1:] - self.fFlux[0:-1]
+        rhsValues = -rhsValues / self.dx
+
+        return rhsValues
+
+
+    def numflux(self, a, b):
+        """
+        Return Lax-Friedrichs numerical flux.
+        """
+        maxval = self.max_flux_deriv(a, b)
+
+        return 0.5 * (self.flux(a) + self.flux(b) - maxval * (b - a))
+
+
+    def get_x_center(self):
+        return self.x_center
