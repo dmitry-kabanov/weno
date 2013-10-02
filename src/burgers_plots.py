1e73de7a14887394ea8cde52be8595cc075113ec
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-10-02 23:38:36 +0300

    Polish script for plotting solutions of Burgers' equation.

--- src/burgers_plots.py
+++ src/burgers_plots.py
@@ -1,27 +1,30 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import burgers as b
-import weno2
+import weno3
 
 def plot_burgers_solution():
-    plt.plot(x_center, solution, 'o', label='WENO, $k = 2$')
+    plt.plot(x_center, solution, 'o', label='WENO, $k = 3$')
     if T <= time_breakage:
         plt.plot(x_center, b.exact_solution(x_center, T, ncells), label='Exact')
-    plt.legend(loc='best')
+    # plt.legend(loc='best')
     plt.ylim([-0.55, 1.55])
     plt.xticks(np.linspace(-1, 1, 11, endpoint=True))
-    plt.show()
-    # plt.savefig('/home/dima/weno2_advection_N=' + str(N) + '_T=' + str(T) + '.eps')
+    plt.title('Time = ' + str(T) + ', ncells = ' + str(ncells) + r', $\nu$ = ' + str(CFL_NUMBER))
+    plt.xlabel(r'$x$')
+    plt.ylabel(r'$u$')
+    # plt.show()
+    plt.savefig('../../images/weno3_burgers_N=' + str(ncells) + '_T=' + str(T) + '.eps')
 
-ncells = 320
+ncells = 640
 left_boundary = -1.0
 right_boundary = 1.0
 CHAR_SPEED = 1.0
 CFL_NUMBER = 0.6
 time_breakage = 0.3183
-T = 0.2
+T = 0.3183
 
-w = weno2.Weno2(left_boundary, right_boundary, ncells, b.flux, b.flux_deriv, b.max_flux_deriv, CHAR_SPEED, CFL_NUMBER)
+w = weno3.Weno3(left_boundary, right_boundary, ncells, b.flux, b.flux_deriv, b.max_flux_deriv, CHAR_SPEED, CFL_NUMBER)
 x_center = w.get_x_center()
 u0 = b.initial_condition(x_center)
 
