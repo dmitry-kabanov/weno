85a1518052f08e58c0ae8008f1505bc2b02b3dc4
Dmitry Kabanov <kabanovdmitry@gmail.com>
2013-10-02 23:31:12 +0300

    Add animation script for Burgers' equation for WENO-3.

---
+++ src/burgers_animation_weno3.py
@@ -0,0 +1,44 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.animation as animation
+import weno3
+import burgers
+
+N = 320
+a = -1.0
+b = 1.0
+CHAR_SPEED = 1.0
+
+w = weno3.Weno3(a, b, N, burgers.flux, burgers.flux_deriv, burgers.max_flux_deriv, CHAR_SPEED, cfl_number=0.6)
+x_center = w.get_x_center()
+u0 = burgers.initial_condition(x_center)
+solution = u0
+
+t = 0.0
+nFrames = 200
+t_final = 3.0
+dt = t_final / nFrames
+
+fig = plt.figure()
+ax = plt.axes(xlim=(-1, 1), ylim=(-0.55, 1.55))
+line, = ax.plot([], [], lw=2)
+time_text = ax.text(0.02, 0.95, '', transform=ax.transAxes)
+
+# initialization function: plot the background of each frame
+def init():
+    line.set_data(x_center, u0)
+    time_text.set_text('time = %s' % 0.0)
+    return line, time_text
+
+# animation function.  This is called sequentially
+def animate(i):
+    global x_center, solution, w
+    solution = w.integrate(solution, dt * i)
+    line.set_data(x_center, solution)
+    time_text.set_text('time = %s' % (dt * i))
+    return line, time_text
+
+anim = animation.FuncAnimation(fig, animate, init_func=init,
+                               frames=nFrames, interval=20, blit=True)
+
+anim.save('../../videos/burgers_weno3.mp4', fps=30)
